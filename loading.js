c8f9a34d49be1e3c9695a302f203df7b19d2eca5
xilanhuadechuntian <Lensgcx@163.com>
2020-02-15 11:52:00 +0800

    3

--- loading.js
+++ loading.js
@@ -1,18 +1,21 @@
-var updateTime = document.getElementById('updateTime');
-var mask = document.getElementById('mask');
-var bottom = document.getElementById('bottom');
-var reapply = document.getElementById('reapply');
-var problem = document.getElementById('problem');
+window.onload = function () {
+    var updateTime = document.getElementById('updateTime');
+    var mask = document.getElementById('mask');
+    var bottom = document.getElementById('bottom');
+    var reapply = document.getElementById('reapply');
+    var problem = document.getElementById('problem');
 
-updateTime.innerText = '更新于：' + getTime();
+    updateTime.innerText = '更新于：' + getTime();
 
 
-bottom.onclick = function () {
-    mask.style.display = 'none';
+    bottom.onclick = function () {
+        mask.style.display = 'none';
+    };
+    reapply.onclick = function () {
+        mask.style.display = 'block';
+    };
+    problem.onclick = function () {
+        window.location.href = 'https://h5.dingtalk.com/healthAct/index.html?corpId=ding888e70e797bb46f1&source=dingHealth&from=singlemessage&isappinstalled=0#/faq';
+    };
 };
-reapply.onclick = function () {
-    mask.style.display = 'block';
-};
-problem.onclick = function () {
-    window.location.href = 'https://h5.dingtalk.com/healthAct/index.html?corpId=ding888e70e797bb46f1&source=dingHealth&from=singlemessage&isappinstalled=0#/faq';
-};
+
